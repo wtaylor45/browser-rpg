506222d6943f5317006244f20b26cea01a685ba7
Will Taylor <taylorw@udel.edu>
2017-09-10 21:21:38 -0400

    NPCs created from list

---
+++ server/js/npcs.js
@@ -0,0 +1,11 @@
+var Npc = require('./npc'),
+    Types = require('../../shared/js/types');
+
+module.exports = Npcs = {
+  man: class Man extends Npc {
+    constructor(x, y){
+      super('0'+x+y, Types.Entities.MAN, x, y, 32, 48, "Man");
+      this.currentSpeed = this.maxSpeed = 7;
+    }
+  }
+}
